cc8e0ac103364fbef68bf0aae4bb3c7b91ffe972
Jhon Sebastian Giraldo Montoya <sebastiangiraldo0917@gmail.com>
2023-03-06 10:40:19 -0500

    first commit

---
+++ database.php
@@ -0,0 +1,12 @@
+<?php
+
+$connection = mysqli_connect(
+  'localhost', 'root', '','u693577563_handymanmede'
+);
+
+// for testing connection
+#if($connection) {
+#  echo 'database is connected';
+#}
+
+?>
